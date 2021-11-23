227f65fad73d148c6cbb7daa5815a002fed162a9
lydianish <nishimwelydia@yahoo.com>
2021-11-23 13:31:24 +0100

    Setup app folder structure

--- mt-challenger/src/main.js
+++ mt-challenger/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import { store } from './store';
+import { router } from './router';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(store)
+app.use(router)
+
+app.mount('#app')
---
+++ mt-challenger/src/router/index.js
@@ -0,0 +1,7 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// there is also createWebHashHistory and createMemoryHistory
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+})
